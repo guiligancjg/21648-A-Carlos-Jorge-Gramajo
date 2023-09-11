b69b1d968a5e192d585f37adc5603fecc6f66371
guiligancjg <guiligan7@gmail.com>
2023-09-11 03:10:45 -0300

    Se agrego la carpeta public

--- app.js
+++ app.js
@@ -1,15 +1,33 @@
-const express = require('express')
-const morgan = require('morgan')
-const app = express()
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+
+require('ejs');
+
+
+const app = express();
+
+// Middlewares
+app.use(express.json());
+app.use(cors(app.js));
+app.use(morgan());
+app.set('view engine', 'ejs');
+
+// Static Files
+app.use(express.static('public'));
+
+// Routes
+const blogRutas = require('./routes/blog.routes');
+app.use(blogRutas);
+
 
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
-app.listen(3000)
+app.listen(3000);
 
 
-// Middlewares
-app.use(express.json());
-app.use(cors());
-app.use(morgan());
+
+
+
